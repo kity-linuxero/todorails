3d3211593ea412b9a67226d2088c7125e248b06e
Cristian Giambruni <cgiambruni@gmail.com>
2016-11-30 20:22:12 -0300

    Test para task

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   
+  # Una tarea bien armada 
+   test "Correct task" do
+	one = Task.new do |t|
+	  t.description= "A new task"
+	  t.created_at = Time.now
+	  t.priority= %w(low medium high).shuffle.first
+	  t.created_at = Time.now
+	  
+	  
+	end
+	assert one.save
+   end
+   
+   # Una tarea con una descripción mas larga de lo permitido
+   test "A long description must be invalid" do
+     one = Task.new do |t|
+	  t.description = "a"*256
+	  t.created_at = Time.now
+	  t.priority = "low"
+	  
+	 end
+	 
+	 assert_not one.save
+   
+   end
+   
+   # Una tarea con una prioridad inválida
+   test "an invalid priority" do
+     one = Task.new do |t|
+	  t.description= "A new task"
+	  t.created_at = Time.now
+	  t.priority = "very low"
+	  
+	 end
+	 
+	 assert_not one.save
+   
+   end
 end
