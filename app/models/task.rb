7049bc7449264aeb413d54aea28e734a011af00e
Cristian Giambruni <cgiambruni@gmail.com>
2016-11-30 19:10:12 -0300

    Validaciones para los modelos task y task_list

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
-  belongs_to :task_list
+  belongs_to :task_list#, inverse_of: :tasks
+  validates :description, presence: true, length: { maximum: 255 }
+  validates :priority, presence: true, inclusion: { in: %w(low medium high),
+  message: "%{value} is not a valid prioriry" }
 end
--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,2 +1,5 @@
 class TaskList < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+  validates :name, presence: true
+  validates :url, presence: true, uniqueness: true
 end
