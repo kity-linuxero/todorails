b43fb34b0aa40e5e25f149c4d2f67843b8b8b001
Cristian Giambruni <cgiambruni@gmail.com>
2016-11-30 20:12:31 -0300

    Test para task_list

--- test/models/task_list_test.rb
+++ test/models/task_list_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class TaskListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   
+   
+   
+   # Una Task List vacía debe ser inválida
+   test "A empty task list must be invalid" do
+     tl = TaskList.new
+     assert_not  tl.valid?
+   end
+   
+   # Una Task List sin una descripción será inválida
+   test "Task without name. Will be invalid" do
+     tl = TaskList.new
+     tl.created_at= Time.now
+     tl.url="url"
+     assert_not tl.save
+   end
+   
+   # No se debe permitir una url repetida
+   test "No repeat URL"do
+   
+     one = TaskList.new do |t|
+	  t.name = "Task List One"
+	  t.created_at = Time.now
+	  t.url = "url1"
+	 end
+	 
+	 one.save
+	 
+	 two = TaskList.new do |t|
+	  t.name = "Task List Two"
+	  t.created_at = Time.now
+	  t.url = "url1"
+	 end
+	 
+	 assert_not two.save
+   
+   end
+   
+   # Un caso feliz :)
+   test "well done" do
+	 one = TaskList.new do |t|
+	  t.name = "Task List One"
+	  t.created_at = Time.now
+	  t.url = "url1"
+	 end
+	 
+	 assert one.save
+	 
+   end
+   
 end
