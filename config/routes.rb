9801f06cb57f2e491bef66f0eb2b4285a1b1054c
Cristian Giambruni <cgiambruni@gmail.com>
2016-12-03 19:53:06 -0300

    Se cambió el root a task_lists#new

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :tasks
   resources :task_lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'task_lists#new'
 end
