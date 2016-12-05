015ce3d45abf5bd187564d4702d0c3dcba3447b0
Cristian Giambruni <cgiambruni@gmail.com>
2016-12-05 03:03:17 -0300

    Purga de rutas redundantes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :tasks, only: [:show, :edit, :destroy, :update]
 
   resources :task_lists do
-    resources :tasks, except: [:index]
+    resources :tasks, except: [:index, :edit, :show, :destroy, :update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'task_lists#new'
