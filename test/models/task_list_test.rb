c3b06118b4ccac5b029590d4f62902b48eebc82b
Cristian Giambruni <cgiambruni@gmail.com>
2016-12-07 15:50:05 -0300

    Test de unidad para el modelo de TaskList

--- test/models/task_list_test.rb
+++ test/models/task_list_test.rb
@@ -1,54 +1,43 @@
 require 'test_helper'
 
 class TaskListTest < ActiveSupport::TestCase
-   
-   
-   
+
    # Una Task List vacía debe ser inválida
    test "A empty task list must be invalid" do
      tl = TaskList.new
      assert_not  tl.valid?
    end
-   
-   # Una Task List sin una descripción será inválida
+
+   # Sin nombre
    test "Task without name. Will be invalid" do
      tl = TaskList.new
-     tl.created_at= Time.now
-     tl.url="url"
+     tl.name= nil
      assert_not tl.save
    end
-   
-   # No se debe permitir una url repetida
-   test "No repeat URL"do
-   
+
+   # Con nombre que respete la unicidad de los slugs.
+   test "slug unicity" do
      one = TaskList.new do |t|
-	  t.name = "Task List One"
-	  t.created_at = Time.now
-	  t.url = "url1"
-	 end
-	 
-	 one.save
-	 
-	 two = TaskList.new do |t|
-	  t.name = "Task List Two"
-	  t.created_at = Time.now
-	  t.url = "url1"
-	 end
-	 
-	 assert_not two.save
-   
+  	  t.name = "Task List One"
+  	  t.created_at = Time.now
+  	 end
+     assert one.save
    end
-   
-   # Un caso feliz :)
-   test "well done" do
-	 one = TaskList.new do |t|
-	  t.name = "Task List One"
-	  t.created_at = Time.now
-	  t.url = "url1"
-	 end
-	 
-	 assert one.save
-	 
+
+   # Con nombre que cause un conflicto de unicidad de los slugs.
+   test "slug unicity conflict"do
+     one = TaskList.new do |t|
+  	  t.name = "Task List One=Ño Ñe %6"
+  	  t.created_at = Time.now
+  	 end
+  	 one.save
+
+  	 two = TaskList.new do |t|
+  	  t.name = "Task List One=Ño Ñe %6"
+  	  t.created_at = Time.now
+  	 end
+  	 two.save
+     assert_not(one.slug == two.slug)
    end
-   
+
 end
