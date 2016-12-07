e252c8c7f5cc604915036c0dfbdb76360d9c20ed
Cristian Giambruni <cgiambruni@gmail.com>
2016-12-07 09:00:14 -0300

    Fix issues #5. En routes.rb se agregó excepción para new. Y en Index de task_lists se redirigirá, cuando lo necesite a la root_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks, only: [:show, :edit, :destroy, :update]
 
-  resources :task_lists do
+  resources :task_lists, except: [:new] do
     resources :tasks, except: [:index, :edit, :show, :destroy, :update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
