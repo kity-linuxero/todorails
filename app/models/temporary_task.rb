c34abb9c7870c995aebfa58fb87dec6980964bc1
Cristian Giambruni <cgiambruni@gmail.com>
2016-12-07 11:28:41 -0300

    expired no es un estado valido para LongTask

--- app/models/temporary_task.rb
+++ app/models/temporary_task.rb
@@ -1,5 +1,5 @@
 class TemporaryTask < Task
-	validates :status, presence: true, inclusion: { in: %w(pending done expired),
+	validates :status, presence: true, inclusion: { in: %w(pending done),
     message: "%{value} is not a valid status for a temporary task" }
 
     validates :start_at, presence: true
