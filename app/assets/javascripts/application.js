6788d7c5a525416c4aab30f5050bb1f17d48973b
Cristian Giambruni <cgiambruni@gmail.com>
2016-12-09 14:51:31 -0300

    Se quitó la opción URL Copy. Dado que no funcionaba bien y daba errores raros con javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,47 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require_tree .
-
-// Place in top-left corner of screen regardless of scroll position.
-textArea.style.position = 'fixed';
-textArea.style.top = 0;
-textArea.style.left = 0;
-
-// Ensure it has a small width and height. Setting to 1px / 1em
-// doesn't work as this gives a negative w/h on some browsers.
-textArea.style.width = '2em';
-textArea.style.height = '2em';
-
-// We don't need padding, reducing the size if it does flash render.
-textArea.style.padding = 0;
-
-// Clean up any borders.
-textArea.style.border = 'none';
-textArea.style.outline = 'none';
-textArea.style.boxShadow = 'none';
-
-// Avoid flash of white box if rendered for any reason.
-textArea.style.background = 'transparent';
-
-
-textArea.value = text;
-
-document.body.appendChild(textArea);
-
-textArea.select();
-
-try {
-  var successful = document.execCommand('copy');
-  var msg = successful ? 'successful' : 'unsuccessful';
-  console.log('Copying text command was ' + msg);
-} catch (err) {
-  console.log('Oops, unable to copy');
-}
-
-document.body.removeChild(textArea);
-}
-
-function CopyLink() {
-copyTextToClipboard(location.href);
-}
+//= require_tree 
