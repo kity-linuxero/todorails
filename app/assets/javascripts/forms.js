6f1d869d554b2ba4f651cf0d4ae981f82efec21f
Cristian Giambruni <cgiambruni@gmail.com>
2017-01-18 11:16:05 -0300

    Task can change type. More improve in forms

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,31 +1,37 @@
+function show_simple_task(delay) {
+  $('#select_long').hide(delay);
+  $('#select_temp').hide(delay);
+  $('#select_status').show(delay);
+}
+
+function show_long_task(delay){
+  $('#select_long').show(delay);
+  $('#select_status').hide(delay);
+  $('#select_temp').hide(delay);
+}
+
+function show_temporary_task(delay){
+  $('#select_temp').show(delay);
+  $('#select_long').hide(delay);
+  $('#select_status').show(delay);
+}
+
 function change() {
+    var delay= 600;
     var selectBox = document.getElementById("selects");
     var selected = selectBox.options[selectBox.selectedIndex].value;
-
     if(selected === 'SimpleTask'){
-        $('#select_long').hide(600);
-        $('#select_temp').hide(600);
-        $('#select_status').show(600);
+      show_simple_task(delay)
     }
     else{
       if(selected === 'LongTask'){
-        $('#select_long').show(600);
-        $('#select_status').hide(600);
-        $('#select_temp').hide(600);
+        show_long_task(delay)
       }
       else {
         if(selected === 'TemporaryTask'){
-          $('#select_temp').show(600);
-          $('#select_long').hide(600);
-          $('#select_status').show(600);
+          show_temporary_task(delay)
         }
       }
 
     }
-}
-
-function simple_default() {
-  $('#select_long').hide(0);
-  $('#select_temp').hide(0);
-  $('#select_status').show(0);
 }
